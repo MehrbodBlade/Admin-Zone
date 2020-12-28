ec02864e9223619f88316cbb6446803bbe4649f4
Mehrbod Blade <75046463+MehrbodBlade@users.noreply.github.com>
2020-12-28 23:47:09 +0330

    Update server.lua

--- AdminZone/server.lua
+++ AdminZone/server.lua
@@ -10,7 +10,7 @@ RegisterCommand('setzone', function(source, args, rawCommand)
 
         if xPlayer.get('aduty') then -- Or any command that admins activate with this device on your server (replace 'aduty')
             local radius = tonumber(args[1])
-            TriggerClientEvent("blade:AdminAreaSet", -1, source, radius)
+            TriggerClientEvent("Blade:AdminAreaSet", -1, source, radius)
         else
             TriggerClientEvent('chatMessage', source, "[SYSTEM]", {255, 0, 0}, " ^0You cant if you are ^1OffDuty ^0Use From this!")
         end
@@ -26,7 +26,7 @@ RegisterCommand('clearzone', function(source, args, rawCommand)
     if xPlayer.permission_level > 1 then
 
         if xPlayer.get('aduty') then -- Or any command that admins activate with this device on your server (replace 'aduty')
-              TriggerClientEvent("blade:AdminAreaClear", -1, source)
+              TriggerClientEvent("Blade:AdminAreaClear", -1, source)
         else
               TriggerClientEvent('chatMessage', source, "[SYSTEM]", {255, 0, 0}, " ^0You cant if you are ^1OffDuty ^0Use From this!")
         end
